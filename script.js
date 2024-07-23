cf7363aa1e42709bd5f176dbdbe9bcf66df3aa32
shanME <giorgie103188@gmail.com>
2024-07-23 16:32:20 +0400

    end

--- script.js
+++ script.js
@@ -5,3 +5,66 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
+
+pupulateUi();
+
+//save selected movie and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+//update total and count
+function updateSelectedCount() {
+  let selectedSeats = document.querySelectorAll(".row .seat.selected");
+  let seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  let selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice + " $";
+}
+
+//movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+//get data from localstorage
+function pupulateUi() {
+  let selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((el, i) => {
+      if (selectedSeats.indexOf(i) > -1) {
+        el.classList.add("selected");
+      }
+    });
+  }
+
+  let selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+
+  let selectedMoviePrice = localStorage.getItem("selectedMoviePrice");
+  if (selectedMoviePrice !== null) {
+    ticketPrice = selectedMoviePrice;
+  }
+}
+
+//seat click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+updateSelectedCount();
